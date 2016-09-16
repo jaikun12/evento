24f9738eae765d85679c12976b5e0c0aab64a2e7
jaikun1 <jaicelim@gmail.com>
2016-09-17 02:34:28 +0800

    wassup

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet"> 
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+
+	</head>
+	<body>
+		<div class="main-content">
+			<center>
+				<img src="images/evento-logo-white.png" class="main-logo">
+				<form class="login-form">
+					<div class="form-group">
+						<input type="text" class="form-control" name="user_name" placeholder="Username">
+					</div>
+					<div class="form-group">
+						<input type="password" class="form-control" name="user_pw" placeholder="Password">
+					</div>
+					<div class="form-group">
+						<button type="submit" class="button-1">Login</button>
+					</div>
+
+
+
+			</center>
+		</div>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-2.2.0.min.js"></script>
+	</body>
+</html>
---
+++ my-events.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet"> 
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/events.css">
+		<link rel="stylesheet" type="text/css" href="css/nav.css">
+
+
+	</head>
+	<body>
+		<nav class="main-nav">
+			<img src="images/evento-logo-white.png" class="brand">
+		</nav>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-2.2.0.min.js"></script>
+	</body>
+</html>
