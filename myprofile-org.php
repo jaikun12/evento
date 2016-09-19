36800067fcd57f1edf95011a8c27fe7d9dc3a1ec
boazcstrike <boazcstrike@yahoo.com.sg>
2016-09-19 21:10:22 +0800

    My profile edits and event tracking

--- myprofile-org.php
+++ myprofile-org.php
@@ -26,6 +26,18 @@
 			DevCon is a non-proﬁt organization which aims to promote the "IT Pinoy Talent" mainly by providing a unique venue for IT students, educators, professionals and enthusiasts to Sync, Support and Succeed.
 			<br><br>Website: <a href="http://devcon.ph/">http://devcon.ph/</a>
 		</div>
+		<hr>
+		<div class="col-md-12">
+		<br><br>
+			<div class="container-fluid" style="background:rgb(39,113,140);">
+				<h4 style="color:white">Most Recent Events:</h4>
+			</div>
+			<div class="container-fluid" style="background:#F2F2F2">
+				<?php include("boilers/event-bluemix.php"); include("boilers/event-card.php"); ?>
+			</div>
+			
+		</div>
+
 
 		</div>
 
